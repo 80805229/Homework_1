9d24ddcfab9df5e4797bb3f8e80a1d4cce9e56e3
Anna <aanikonova15@gmail.com>
2022-02-07 21:39:30 +0300

    Add Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трёх чисел.
+
+int a = 3;
+int b = 10;
+int c = 19;
+
+int max = a;
+
+if (max < a) max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+
+Console.Write("Максимальное число равно ");
+Console.WriteLine(max);
