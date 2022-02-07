2bee5d3d4bcb252fd94bc1b259e4559aadee62cc
Anna <aanikonova15@gmail.com>
2022-02-07 21:58:09 +0300

    Add Task_06

---
+++ Task_06/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным
+
+int num = 11;
+Console.Write("Введенное число ");
+Console.WriteLine(num);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
